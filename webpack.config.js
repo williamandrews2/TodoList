dbe9e2acfef7b75a364c1ba34a926ff5b1918e9b
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-12-26 17:21:02 -0600

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|webp|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
