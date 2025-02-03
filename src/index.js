68db0b8bb7ac7bc2fb1008c8fc0d7b3905a9020b
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-02-02 20:15:32 -0600

    Changed projectController.js to be a singleton for ease of use in index.js.

--- src/index.js
+++ src/index.js
@@ -4,20 +4,9 @@ import createToDo from "./modules/createTodo";
 import createProject from "./modules/project";
 import projectController from "./modules/projectController";
 
-// const todo = createToDo("task1", "go to grocery store", "01/31/2025", "high");
+const workProject = projectController.addProject("Work");
 
-const myProject = createProject("Work");
-
-const newTodo = createToDo(
-  "Go shopping",
-  "Hit up Sprouts and Target",
-  "Tomorrow at 1PM",
-  "High"
-);
-
-myProject.addTodo(newTodo);
-console.log(newTodo);
-console.log(myProject.todos);
+console.log(projectController.getAllProjects());
 
 // TODO: Create and load a default project here.
 
--- src/modules/projectController.js
+++ src/modules/projectController.js
@@ -1,52 +1,38 @@
 import createProject from "./project";
 
-const projectController = () => {
-  let projects = []; // Store all the projects here.
-  let currentProject = null;
+const projects = []; // Store all projects here.
+let currentProject = null; // Track the currently active project.
 
-  const addProject = (name) => {
+const addProject = (name) => {
+  if (!projects.some((project) => project.name === name)) {
     const newProject = createProject(name);
     projects.push(newProject);
 
-    // Set to current project if this is the first project being created.
     if (!currentProject) {
       currentProject = newProject;
     }
-
-    return newProject;
-  };
-
-  // Find a project by name:
-  const getProject = (name) => {
-    return projects.find((project) => project.name === name);
-  };
-
-  // Delete a project by name:
-  const deleteProject = (name) => {
-    // Update the array of projects:
-    projects = projects.filter((project) => project.name !== name);
-
-    // Set the another project to be the current project if the
-    // current project was the one that was deleted.
-    if (currentProject && currentProject.name === name) {
-      currentProject = projects.length > 0 ? projects[0] : null;
-    }
-  };
-
-  const setCurrentProject = (name) => {
-    const project = getProject(name);
-    if (project) {
-      currentProject = project;
-    }
-  };
-
-  const getCurrentProject = () => {
-    return currentProject;
-  };
-
-  const getAllProjects = () => {
-    return projects;
-  };
+  }
 };
 
-export default projectController;
+const getProject = (name) => projects.find((project) => project.name === name);
+const deleteProject = (name) => {
+  projects = projects.filter((project) => project.name !== name);
+  if (currentProject && currentProject.name === name) {
+    currentProject = projects.length > 0 ? projects[0] : null;
+  }
+};
+const setCurrentProject = (name) => {
+  const project = getProject(name);
+  if (project) currentProject = project;
+};
+const getCurrentProject = () => currentProject;
+const getAllProjects = () => projects;
+
+export default {
+  addProject,
+  getProject,
+  deleteProject,
+  setCurrentProject,
+  getCurrentProject,
+  getAllProjects,
+};
