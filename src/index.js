553852f5c44ce5b3bf70abcc97291e250e5afe22
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-02-08 22:07:06 -0600

    Created basic functionality to add and render todos.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,58 @@
 import "./styles.css";
 
 import createToDo from "./modules/createTodo";
+import createProject from "./modules/project";
 import projectController from "./modules/projectController";
 import ui from "./modules/ui";
 
 // TESTING:
-// const workProject = projectController.addProject("Work");
-// const workProject2 = projectController.addProject("Work2");
-// const workProject3 = projectController.addProject("Work3");
+projectController.addProject("Work");
+projectController.addProject("Work2");
+projectController.addProject("Work3");
+
+const todo1 = createToDo(
+  "Go to work",
+  "Arrive at work ready to work.",
+  "02/10/2025",
+  "Medium"
+);
+
+const todo2 = createToDo(
+  "Go grocery shopping",
+  "Purchase milk, eggs, and yogurt.",
+  "02/15/2025",
+  "Medium"
+);
+
+const todo3 = createToDo(
+  "Plan next vacation",
+  "Create plans for this next year.",
+  "07/23/2025",
+  "Low"
+);
+
+// Todos must be added like this for now since we do not have access to direct variable
+projectController.getProject("Work").addTodo(todo1);
+projectController.getProject("Work").addTodo(todo2);
+projectController.getProject("Work").addTodo(todo3);
+
+// END TESTING
 
 const dashboardButton = document.getElementById("dashboard-btn");
+const projectsButton = document.getElementById("projects-btn");
+const addTaskButton = document.getElementById("add-task-btn");
 
-function showDashboard() {
+function showDashboard() {}
+
+function showProjects() {
   ui.renderProjectList();
-  console.log("Showing dashboard and project list!");
 }
-
-function showProjects() {}
 
 function addTask() {}
 
 dashboardButton.addEventListener("click", showDashboard);
+projectsButton.addEventListener("click", showProjects);
+addTaskButton.addEventListener("click", addTask);
 
 // TODO: Create and load a default project here.
 
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -2,7 +2,7 @@ import projectController from "./projectController";
 
 const content = document.querySelector("#content");
 
-// render project list
+// render project list (used by the "Projects" button)
 const renderProjectList = () => {
   content.innerHTML = "";
   createGreeting();
@@ -11,19 +11,70 @@ const renderProjectList = () => {
   content.appendChild(projectContainer);
   const projectList = projectController.getAllProjects();
   projectList.forEach((project) => {
-    const projectListItem = document.createElement("div"); // Might need to change to buttons...
+    const projectListItem = document.createElement("button");
+    projectListItem.addEventListener("click", () =>
+      renderProject(project.name)
+    );
     projectListItem.className = "project-list-item";
     projectListItem.innerHTML = project.name;
     projectContainer.appendChild(projectListItem);
-    console.log(project.name);
   });
 };
 
 // render project and its contents
-const renderProject = () => {};
+const renderProject = (name) => {
+  projectController.setCurrentProject(name);
+  content.innerHTML = "";
+  showBackButton();
+  // show the title of the project
+  const projectName = document.createElement("h1");
+  projectName.innerText = name;
+  content.appendChild(projectName);
+  // show project attributes
+  renderTodos();
+};
 
 // render todos
-const renderTodos = () => {};
+const renderTodos = () => {
+  const project = projectController.getCurrentProject();
+  let numTodos = project.todos.length; // number of todos in the current project
+  if (numTodos != 0) {
+    const todoItemContainer = document.createElement("div");
+    todoItemContainer.id = "todo-item-container";
+    content.appendChild(todoItemContainer);
+    for (let i = 0; i < numTodos; i++) {
+      console.log(
+        `Task: ${project.todos[i].title}; Description: ${project.todos[i].description}; Due date: ${project.todos[i].dueDate}; Priority: ${project.todos[i].priority}`
+      );
+      // todo item container
+      const todoItem = document.createElement("div");
+      todoItem.className = "todo-item";
+      // title
+      const title = document.createElement("h2");
+      title.innerText = `${project.todos[i].title}`;
+      todoItem.appendChild(title);
+      // description
+      const description = document.createElement("h4");
+      description.innerText = `${project.todos[i].description}`;
+      todoItem.appendChild(description);
+      // due date
+      const dueDate = document.createElement("h4");
+      dueDate.innerText = `Due: ${project.todos[i].dueDate}`;
+      todoItem.appendChild(dueDate);
+      // priority
+      const priority = document.createElement("h4");
+      priority.innerText = `Priority: ${project.todos[i].priority}`;
+      todoItem.appendChild(priority);
+      // append to container
+      todoItemContainer.appendChild(todoItem);
+    }
+  } else {
+    const noTodoMessage = document.createElement("h3");
+    noTodoMessage.innerText = "No todos yet!";
+    // add link/button here to create the user's first todo.
+    content.appendChild(noTodoMessage);
+  }
+};
 
 // greet the user
 const createGreeting = () => {
@@ -31,6 +82,15 @@ const createGreeting = () => {
   greeting.id = "greeting";
   greeting.innerText = "Hello";
   content.appendChild(greeting);
+};
+
+const showBackButton = () => {
+  // This function returns to project page only. No
+  // functionality for returning to another page yet.
+  const backButton = document.createElement("button");
+  backButton.innerText = "Back to Projects";
+  content.appendChild(backButton);
+  backButton.addEventListener("click", () => renderProjectList());
 };
 
 export default {
