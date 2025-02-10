d75b713519eed598e1b01f83f8864bc5397d0d28
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2025-02-09 23:37:28 -0600

    Added information to the dashboard.

--- src/index.js
+++ src/index.js
@@ -13,36 +13,40 @@ projectController.addProject("Work3");
 const todo1 = createToDo(
   "Go to work",
   "Arrive at work ready to work.",
-  "02/10/2025",
+  "02-10-2025",
   "Medium"
 );
 
 const todo2 = createToDo(
   "Go grocery shopping",
   "Purchase milk, eggs, and yogurt.",
-  "02/15/2025",
+  "02-09-2025",
   "Medium"
 );
 
 const todo3 = createToDo(
   "Plan next vacation",
   "Create plans for this next year.",
-  "07/23/2025",
+  "07-23-2025",
   "Low"
 );
 
-// Todos must be added like this for now since we do not have access to direct variable
 projectController.getProject("Work").addTodo(todo1);
 projectController.getProject("Work").addTodo(todo2);
 projectController.getProject("Work").addTodo(todo3);
 
 // END TESTING
+document.addEventListener("DOMContentLoaded", () => {
+  showDashboard();
+});
 
 const dashboardButton = document.getElementById("dashboard-btn");
 const projectsButton = document.getElementById("projects-btn");
 const addTaskButton = document.getElementById("add-task-btn");
 
-function showDashboard() {}
+function showDashboard() {
+  ui.renderDashboard();
+}
 
 function showProjects() {
   ui.renderProjectList();
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,8 +1,10 @@
 import projectController from "./projectController";
+import { format } from "date-fns";
+import { parse } from "date-fns";
+import { isSameDay } from "date-fns";
 
 const content = document.querySelector("#content");
 
-// render project list (used by the "Projects" button)
 const renderProjectList = () => {
   content.innerHTML = "";
   createGreeting();
@@ -12,16 +14,16 @@ const renderProjectList = () => {
   const projectList = projectController.getAllProjects();
   projectList.forEach((project) => {
     const projectListItem = document.createElement("button");
+    projectListItem.className = "project-list-buttons";
     projectListItem.addEventListener("click", () =>
       renderProject(project.name)
     );
     projectListItem.className = "project-list-item";
-    projectListItem.innerHTML = project.name;
+    projectListItem.innerHTML = `${project.name} (${project.todos.length} items)`;
     projectContainer.appendChild(projectListItem);
   });
 };
 
-// render project and its contents
 const renderProject = (name) => {
   projectController.setCurrentProject(name);
   content.innerHTML = "";
@@ -34,7 +36,6 @@ const renderProject = (name) => {
   renderTodos();
 };
 
-// render todos
 const renderTodos = () => {
   const project = projectController.getCurrentProject();
   let numTodos = project.todos.length; // number of todos in the current project
@@ -43,9 +44,6 @@ const renderTodos = () => {
     todoItemContainer.id = "todo-item-container";
     content.appendChild(todoItemContainer);
     for (let i = 0; i < numTodos; i++) {
-      console.log(
-        `Task: ${project.todos[i].title}; Description: ${project.todos[i].description}; Due date: ${project.todos[i].dueDate}; Priority: ${project.todos[i].priority}`
-      );
       // todo item container
       const todoItem = document.createElement("div");
       todoItem.className = "todo-item";
@@ -88,13 +86,65 @@ const showBackButton = () => {
   // This function returns to project page only. No
   // functionality for returning to another page yet.
   const backButton = document.createElement("button");
+  backButton.className = "button-style-1";
   backButton.innerText = "Back to Projects";
   content.appendChild(backButton);
   backButton.addEventListener("click", () => renderProjectList());
+};
+
+const getTotalTodos = () => {};
+
+const getCurrentDate = () => {
+  return format(new Date(), "MM-dd-yyyy"); // Formats today's date as MM-DD-YYYY
+};
+
+const getTodosToday = () => {
+  const allProjects = projectController.getAllProjects();
+  const today = getCurrentDate();
+  let dueTodayTodos = [];
+  allProjects.forEach((project) => {
+    const filteredTodos = project.todos.filter((todo) => {
+      const todoDate = parse(todo.dueDate, "MM-dd-yyyy", new Date());
+      console.log(todoDate);
+      return isSameDay(todoDate, today); // Check if due today
+    });
+
+    dueTodayTodos = dueTodayTodos.concat(filteredTodos);
+  });
+  return dueTodayTodos;
+};
+
+const renderDashboard = () => {
+  content.innerHTML = "";
+  createGreeting();
+  // create a random quote selector here.
+  const quoteContainer = document.createElement("div");
+  quoteContainer.id = "quote-container";
+  quoteContainer.innerText = `"What you do today can improve all your tomorrows." - Ralph Marston`;
+  content.appendChild(quoteContainer);
+  const today = document.createElement("h2");
+  today.innerText = "Today's tasks";
+  content.appendChild(today);
+
+  const dueTodayTodos = getTodosToday();
+  if (dueTodayTodos.length === 0) {
+    const noTodosToday = document.createElement("div");
+    noTodosToday.innerText = "No tasks due today!";
+    content.appendChild(noTodosToday);
+  } else {
+    const todoList = document.createElement("ul");
+    dueTodayTodos.forEach((todo) => {
+      const todoItem = document.createElement("li");
+      todoItem.innerText = `${todo.title} - ${todo.description}`;
+      todoList.appendChild(todoItem);
+    });
+    content.appendChild(todoList);
+  }
 };
 
 export default {
   renderProjectList,
   renderProject,
   renderTodos,
+  renderDashboard,
 };
